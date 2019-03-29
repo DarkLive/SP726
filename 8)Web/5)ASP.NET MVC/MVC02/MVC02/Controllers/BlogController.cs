6f82f2351f6626657738f5ae05d0aa0813cca361
DarkLive <darklive_@live.com>
2019-03-29 13:06:30 +0300

    MVC02

---
+++ 8)Web/5)ASP.NET MVC/MVC02/MVC02/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC02.Models;
+
+namespace MVC02.Controllers {
+    public class BlogController : Controller {
+        // GET: Blog
+        public ActionResult Index() {
+            return View((new DBEntities()).Articles.ToList());
+        }
+
+        public ActionResult Add() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Article art, HttpPostedFileBase articlepic) {
+            art.artImage = $"{DateTime.Now.ToShortDateString()}_{Guid.NewGuid().ToString().Replace("-","")}_{articlepic.FileName}";
+            articlepic.SaveAs(Server.MapPath("~/ " + art.artImage));
+            art.artUpdateDate = art.artPublishDate;
+            DBEntities Cord = new DBEntities();
+            Cord.Articles.Add(art);
+            Cord.SaveChanges();
+            return Redirect("Index");
+        }
+    }
+}

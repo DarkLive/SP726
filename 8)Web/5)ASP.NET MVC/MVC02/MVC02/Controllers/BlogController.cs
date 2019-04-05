2a7572d5179bf8a1febab9d19af74a981cfd2c23
DarkLive <darklive_@live.com>
2019-04-05 13:59:54 +0300

    MVC02 Update 06

--- 8)Web/5)ASP.NET MVC/MVC02/MVC02/Controllers/BlogController.cs
+++ 8)Web/5)ASP.NET MVC/MVC02/MVC02/Controllers/BlogController.cs
@@ -17,14 +17,28 @@ namespace MVC02.Controllers {
         }
 
         [HttpPost]
-        public ActionResult Add(Article art, HttpPostedFileBase articlepic) {
+        public ActionResult Add(Article art, HttpPostedFileBase articlepic, int[] selectedCat) {
             art.artPublishDate = DateTime.Now; art.artUpdateDate = art.artPublishDate;
-            art.artImage = $"img/content/article/{DateTime.Now.ToShortDateString()}_{Guid.NewGuid().ToString().Replace("-","")}_{articlepic.FileName}";
+            art.artImage = $"img/content/article/{DateTime.Now.ToShortDateString()}_{Guid.NewGuid().ToString().Replace("-", "")}_{articlepic.FileName}";
             articlepic.SaveAs(Server.MapPath("~/" + art.artImage));
 
-            DBEntities Cord = new DBEntities();
-            Cord.Articles.Add(art);
-            Cord.SaveChanges();
+            {
+                DBEntities Cord = new DBEntities();
+                Cord.Articles.Add(art);
+                Cord.SaveChanges();
+            }
+
+            //Cat
+            if (selectedCat!=null)
+            foreach (var item in selectedCat) {
+                DBEntities Cord = new DBEntities();
+                Rel_Article_Category rel = new Rel_Article_Category();
+                rel.relArtID = art.artID;
+                rel.relCatID = item;
+                Cord.Rel_Article_Category.Add(rel);
+                Cord.SaveChanges();
+            }
+
             return RedirectToAction("Index");
         }
     }
